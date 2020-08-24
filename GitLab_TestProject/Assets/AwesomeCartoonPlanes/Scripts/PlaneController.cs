b81d49108fe80e25ca3e883d93647cc7c5b3456d
RedWraith <brianpurdy01@gmail.com>
2020-08-24 00:28:29 -0400

    Made the terrain expand alot more
    added more terrain by duplacation for testing experience while in flight

---
+++ GitLab_TestProject/Assets/AwesomeCartoonPlanes/Scripts/PlaneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    [SerializeField] private float speed = 120.0f;
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+    }
+}
